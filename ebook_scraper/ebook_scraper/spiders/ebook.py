4f09e5dff91663fa8f3c6cac517e2f3c89ce64ce
gashawgedef@gmail.com <gashawgedef@gmail.com>
2024-07-03 03:29:36 +0300

    web scrapping using scrapy

---
+++ ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -0,0 +1,22 @@
+from turtle import title
+from typing import Any
+import scrapy
+from scrapy.http import Response
+
+class EbookSpider(scrapy.Spider):
+    name = "ebook"
+    start_urls = ["https://books.toscrape.com/"]
+    
+    def parse(self, response: Response):
+         article_html = response.css("article")
+         for ebook in article_html:
+             title = ebook.css("a::text").get()
+             price = ebook.css("p.price_color::text").get()
+             yield {
+                 "title":title,
+                 "price":price
+             }
+            
+             
+        #  print(article_html)
+     
