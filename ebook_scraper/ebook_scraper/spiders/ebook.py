c903b62f484d6285950593e8a809e81c05e9d799
gashawgedef@gmail.com <gashawgedef@gmail.com>
2024-07-03 04:15:11 +0300

    web scrapping with scrapty css selector and x path

--- ebook_scraper/ebook_scraper/spiders/ebook.py
+++ ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -8,14 +8,17 @@ class EbookSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com/"]
     
     def parse(self, response: Response):
-         article_html = response.css("article")
-         for ebook in article_html:
-             title = ebook.css("a::text").get()
-             price = ebook.css("p.price_color::text").get()
-             yield {
-                 "title":title,
-                 "price":price
-             }
+        print(["parse"])
+        print( response.css("h3 a::text")[0])
+        print(response.xpath("//h3/a/text()")[0])
+        #  article_html = response.css("article")
+        #  for ebook in article_html:
+        #      title = ebook.css("a::text").get()
+        #      price = ebook.css("p.price_color::text").get()
+        #      yield {
+        #          "title":title,
+        #          "price":price
+        #      }
             
              
         #  print(article_html)
