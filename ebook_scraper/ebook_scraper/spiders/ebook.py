07b6bb9431b737f90537d8dc809b5c42019e9f62
gashawgedef@gmail.com <gashawgedef@gmail.com>
2024-07-05 02:36:29 +0300

    scrappy paginations

--- ebook_scraper/ebook_scraper/spiders/ebook.py
+++ ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -1,4 +1,5 @@
 from turtle import title
+from typing import Iterable
 from unittest import loader
 from ebook_scraper.items import EbookItem
 from scrapy.loader import ItemLoader
@@ -8,17 +9,51 @@ from scrapy.http import Response
     
 class EbookSpider(scrapy.Spider):
     name = "ebook"
-    start_urls = ["https://books.toscrape.com/catalogue/category/books/travel_2/"]
+    # start_urls = ["https://books.toscrape.com/catalogue/category/books/sequential-art_5"]
     cols = ["Title","Price"]
+    
+   
+        
+    def __init__(self):
+        super().__init__()
+        # self.page_count = 0
+        self.page_count = 1
+        self.total_pages = 4
+        
+    def start_requests(self):
+        base_url = "https://books.toscrape.com/catalogue/category/books/sequential-art_5"
+        while self.page_count <= self.total_pages:
+            yield scrapy.Request(
+                f"{base_url}/page-{self.page_count}.html"
+            )
+            self.page_count +=1
     def parse(self, response: Response):
-       ebooks =response.css("article.product_pod")
-       for ebook in ebooks:
+        ebooks =response.css("article.product_pod")
+        for ebook in ebooks:
+            
+           
+           
         #    ebook_item = EbookItem()
-        loader = ItemLoader(item=EbookItem(),selector=ebook)
-        loader.add_css('title',"h3 a::attr(title)")
-        loader.add_css('price','p.price_color::text')
-        #    ebook_item['title'] = ebook.css("h3 a").attrib['title']
-        #    ebook_item['price'] = get_price(ebook.css('p.price_color::text').get())
-        yield loader.load_item()
+            loader = ItemLoader(item=EbookItem(),selector=ebook)
+            loader.add_css('title',"h3 a::attr(title)")
+            loader.add_css('price','p.price_color::text')
+            #    ebook_item['title'] = ebook.css("h3 a").attrib['title']
+            #    ebook_item['price'] = get_price(ebook.css('p.price_color::text').get())
+            yield loader.load_item()
+        print("[PAGE COUNT:]",self.page_count)
+    #    next_btn = response.css("li.next a")
+    #    if next_btn:
+    #        next_page_url = next_btn.attrib["href"]
+    #        print("[Next Page Url:]", next_page_url)
+    #    else:
+    #     print("[Next Page Url:] None")
+    #Takes the href value and attach to the main url
+    
+        # next_btn  =  response.css("li.next a")
+        # if next_btn:
+        #      next_page =  f"{self.start_urls[0]}/{next_btn.attrib["href"]}"
+        #      yield scrapy.Request(url=next_page)
+            
+    
         
        
