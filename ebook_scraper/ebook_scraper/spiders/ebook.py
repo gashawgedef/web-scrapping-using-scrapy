c1915e94660301388d5699d1b2e4329b6f2c6ce4
gashawgedef@gmail.com <gashawgedef@gmail.com>
2024-07-05 04:07:05 +0300

    commits massges for comment

--- ebook_scraper/ebook_scraper/spiders/ebook.py
+++ ebook_scraper/ebook_scraper/spiders/ebook.py
@@ -30,18 +30,19 @@ class EbookSpider(scrapy.Spider):
     def parse(self, response: Response):
         ebooks =response.css("article.product_pod")
         for ebook in ebooks:
+            """The functionality is scrapping details following the link"""
             url = ebook.css('h3 a').attrib['href']
             yield scrapy.Request(
                 url=self.start_urls[0]+url,
                 callback= self.parse_details
             )
-            
-        #    ebook_item = EbookItem()
-            
-            #    ebook_item['title'] = ebook.css("h3 a").attrib['title']
-            #    ebook_item['price'] = get_price(ebook.css('p.price_color::text').get())
-            # yield loader.load_item()
-            
+            """
+            Get tile and price from the main web site
+            ebook_item = EbookItem()
+            ebook_item['title'] = ebook.css("h3 a").attrib['title']
+            ebook_item['price'] = get_price(ebook.css('p.price_color::text').get())
+            yield loader.load_item()
+            """
     def parse_details(self,response):
         main = response.css("div.product_main")
         loader = ItemLoader(item=EbookItem(),selector=main)
@@ -50,20 +51,23 @@ class EbookSpider(scrapy.Spider):
         quantity_p = main.css("p.availability")
         loader.add_value("quantity",quantity_p.re(r'\(.+ available\)')[0])
         yield loader.load_item()
-        # print("[PAGE COUNT:]",self.page_count)
-    #    next_btn = response.css("li.next a")
-    #    if next_btn:
-    #        next_page_url = next_btn.attrib["href"]
-    #        print("[Next Page Url:]", next_page_url)
-    #    else:
-    #     print("[Next Page Url:] None")
-    #Takes the href value and attach to the main url
+        
+        """
+        The functionality is scrapping base web site using pagination
+        print("[PAGE COUNT:]",self.page_count)
+       next_btn = response.css("li.next a")
+       if next_btn:
+           next_page_url = next_btn.attrib["href"]
+           print("[Next Page Url:]", next_page_url)
+       else:
+        print("[Next Page Url:] None")
+    Takes the href value and attach to the main url
     
-        # next_btn  =  response.css("li.next a")
-        # if next_btn:
-        #      next_page =  f"{self.start_urls[0]}/{next_btn.attrib["href"]}"
-        #      yield scrapy.Request(url=next_page)
+        next_btn  =  response.css("li.next a")
+        if next_btn:
+             next_page =  f"{self.start_urls[0]}/{next_btn.attrib["href"]}"
+             yield scrapy.Request(url=next_page)
             
-    
+    """
         
        
